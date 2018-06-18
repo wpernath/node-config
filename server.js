91867c8664305222218abdbc7a1bd2600d0703ef
Wanja Pernath <w.pernath@gmx.de>
2018-06-18 17:48:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var util = require('util');
 var http = require('http');
 var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var propertiesFile = process.env.CONFIG_FILE || '/etc/node-app/node-app.config';
 
 var PropertiesReader = require('properties-reader');
 
@@ -10,7 +11,7 @@ var server = http.createServer(function (req, res) {
    req.on('data', function (data) {});
    req.on('end', function () {
       console.log("Invoked");
-      var properties = PropertiesReader('/etc/node-app/node-app.config');
+      var properties = PropertiesReader(propertiesFile);
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write('<html><head><title>' + properties.get('title') + '</title></head>');
       res.write('<body bgcolor="' + properties.get('color') + '"><br/><br/>');
